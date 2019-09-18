094ec34a5f257bb4128def6db9e795f5e3411fed
Manu <black_falcon00@hotmail.com>
2019-09-18 15:12:26 +0200

    Done

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Rigidbody2D hook;
+    public float releaseTime = .15f;
+    public float maxDragDistance = 2f;
+
+    public GameObject nextBall;
+
+    private bool isPressed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPressed == true)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector3.Distance(mousePos, hook.position) > maxDragDistance)
+            {
+                rb.position = hook.position + (mousePos - hook.position).normalized * maxDragDistance;
+            }
+            else
+            {
+                rb.position = mousePos;
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        isPressed = true;
+        rb.bodyType = RigidbodyType2D.Kinematic;
+    }
+
+    private void OnMouseUp()
+    {
+        isPressed = false;
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        StartCoroutine(Release());
+    }
+
+    private IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseTime);
+
+        GetComponent<SpringJoint2D>().enabled = false;
+        this.enabled = false;
+
+        yield return new WaitForSeconds(2f);
+
+        if (nextBall != null)
+        {
+            nextBall.SetActive(true);
+        }
+        else
+        {
+            Enemy.EnemiesAlive = 0; // Otherwise the static variable is not reinitialised at Loadscene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float health = 4f; // Actually velocity at which collision is managed.
+    public GameObject deathEffect;
+
+    public static int EnemiesAlive = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnemiesAlive++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.magnitude > health)
+        {
+            Die();
+        }
+
+        //Debug.Log(collision.relativeVelocity.magnitude);
+    }
+
+    private void Die()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        EnemiesAlive--;
+        if (EnemiesAlive <= 0)
+        {
+            Debug.Log("Level won");
+        }
+
+        Destroy(gameObject);
+    }
+}
